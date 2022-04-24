8371194b3c1734ee0db381cfaa78a41409a632bf
Hertanto Irawan <hertanto.irawan@outlook.com>
2022-04-25 00:50:32 +0700

    Edit connection for heroku

--- models/dbConnect.js
+++ models/dbConnect.js
@@ -2,12 +2,26 @@ import pg from 'pg';
 
 // Initialise DB connection
 const { Pool } = pg;
-const pgConnectionConfigs = {
-  user: 'hirawan',
-  host: 'localhost',
-  database: 'zodiac_wealth',
-  port: 5432, // Postgres server always runs on this port
-};
+
+let pgConnectionConfigs;
+
+// test to see if the env var is set. Then we know we are in Heroku
+if (process.env.DATABASE_URL) {
+  // pg will take in the entire value and use it to connect
+  pgConnectionConfigs = {
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+      rejectUnauthorized: false,
+    },
+  };
+} else {
+  pgConnectionConfigs = {
+    user: 'hirawan',
+    host: 'localhost',
+    database: 'zodiac_wealth',
+    port: 5432,
+  };
+}
 const pool = new Pool(pgConnectionConfigs);
 
 export default pool;
